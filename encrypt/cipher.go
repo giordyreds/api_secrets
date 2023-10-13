99280ac25494467ed70905b005cc88ec2d88a02e
giordyreds <giordy.rossi@gmail.com>
2023-10-14 01:09:01 +0200

    added encryption and decription functions

---
+++ encrypt/cipher.go
@@ -0,0 +1,73 @@
+package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+)
+
+func Encrypt(key string, plaintext string) (string, error) {
+	block, err := newCipherBlock(key)
+
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
+
+	return fmt.Sprintf("%x", ciphertext), nil
+}
+
+func Decrypt(key string, cipherHex string) (string, error) {
+	block, err := newCipherBlock(key)
+
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext, err := hex.DecodeString(cipherHex)
+
+	if err != nil {
+		return "", err
+	}
+
+	// no initialization vector here
+	if len(ciphertext) < aes.BlockSize {
+		return "", errors.New("encrypt: cipher too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	// in this way you add some salt to ciphered text,
+	// if someone finds enc key he cannot retrieve the text
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext), nil
+}
+
+func newCipherBlock(key string) (cipher.Block, error) {
+	hasher := md5.New()
+	fmt.Fprint(hasher, key)
+	cipher_key := hasher.Sum(nil)
+	block, err := aes.NewCipher(cipher_key)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return block, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/apisecrets
+
+go 1.21.3
